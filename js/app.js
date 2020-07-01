c0f0a83fa59ef6f910dd8796deb6c5f69199c15c
chipres <ana.chipres16@tectijuana.edu.mx>
2020-06-30 17:48:23 -0700

    mejorandoproye

---
+++ js/app.js
@@ -0,0 +1,121 @@
+//a
+var url = window.location.href;
+var swLocation = '/smeraki/sw.js';
+
+
+if ( navigator.serviceWorker ) {
+
+
+    if ( url.includes('localhost') ) {
+        swLocation = '/sw.js';
+    }
+
+
+    navigator.serviceWorker.register( swLocation );
+}
+
+
+
+
+
+//--------------------------------------.------audio-mesiento--------------------------------------------------------------//
+
+	
+var audioBien = new Audio();
+audioBien.src = "audio/tabla1mesiento/Bien.mp3";
+
+var audioMal = new Audio();
+audioMal.src = "audio/tabla1mesiento/Mal.mp3";
+
+var audioTriste = new Audio();
+audioTriste.src = "audio/tabla1mesiento/Triste.mp3";
+
+var audioContento = new Audio();
+audioContento.src = "audio/tabla1mesiento/Contento.mp3";
+
+var audioEnfadado = new Audio();
+audioEnfadado.src = "audio/tabla1mesiento/Enfadado.mp3";
+
+var audioEnfermo = new Audio();
+audioEnfermo.src = "audio/tabla1mesiento/Enfermo.mp3";
+
+var audioCansado = new Audio();
+audioCansado.src = "audio/tabla1mesiento/Cansado.mp3";
+
+var audioAsustado = new Audio();
+audioAsustado.src = "audio/tabla1mesiento/Asustado.mp3";
+
+
+
+
+//------------------------------------audio-index (Tabla1)--------------------------------------------------//
+
+
+var audioTematica = new Audio();
+audioTematica.src = "audio/Tematica.mp3";
+var audioHola = new Audio();
+audioHola.src = "audio/tab1principal/Hola.mp3";
+var audioAdios = new Audio();
+audioAdios.src = "audio/tab1principal/Adios.mp3";
+
+
+var audioSi = new Audio();
+audioSi.src = "audio/tab1principal/si.mp3";
+
+function comoEstas() {
+	var audioComoe = new Audio();
+		audioComoe.src = "audio/tab1principal/Comoe.mp3";
+		audioComoe.play();
+
+		setTimeout(function(){ 
+			var myWindow = location.replace('me-siento.html','_self',"scrollbars=no");
+		}, 1000);
+}
+
+var audioGracias = new Audio();
+audioGracias.src = "audio/tab1principal/Gracias.mp3";
+
+function irTabla1b() {
+	var myWindow = location.replace('index.html',"scrollbars=no");
+		
+}
+
+var audioNo = new Audio();
+audioNo.src = "audio/tab1principal/No.mp3";
+
+function meSiento() {
+	var audioMesiento = new Audio();
+		audioMesiento.src = "audio/tab1principal/Mesiento.mp3";
+		audioMesiento.play();
+
+		setTimeout(function(){ 
+			var myWindow = location.replace('me-siento.html','_self',"scrollbars=no");
+		}, 1000);
+}
+
+var audioSobremi = new Audio();
+audioSobremi.src = "audio/tab1principal/Sobremi.mp3";
+
+var audioTequiero = new Audio();
+audioTequiero.src = "audio/tab1principal/Tequiero.mp3";
+
+
+
+	/*Funcion para regresar a la pagina principal con flecha a la izquierda*/
+	function regresaTabla1() {
+		var myWindow = location.replace('index.html','_self',"scrollbars=no");	
+	}
+
+	/*Funcion para regresar a la pagina principal con icono de casita*/
+	function Inicio() {
+			var myWindow = location.replace('index.html',"scrollbars=no");
+	}
+
+	//Flecha abajo en tematica
+
+	function irflechaa() {
+	var myWindow = location.replace('index.html',"scrollbars=no");
+		
+	}
+
+
---
+++ sw.js
@@ -0,0 +1,137 @@
+// imports
+importScripts('js/sw-utils.js');
+
+
+const STATIC_CACHE    = 'static-v1';
+const DYNAMIC_CACHE   = 'dynamic-v1';
+const INMUTABLE_CACHE = 'inmutable-v1';
+
+
+const APP_SHELL = [
+    // '/',
+    'index.html',
+    'css/estilo.css',
+    'me-siento.html',
+    'img/favicon.ico',
+    
+    'img/imagenestabla1/Adios.jpg',
+    'img/imagenestabla1/Como-estas.jpg',
+    'img/imagenestabla1/Gracias.jpg',
+    'img/imagenestabla1/Hola.jpg',
+    'img/imagenestabla1/Me-siento.jpg',
+    'img/imagenestabla1/No.jpg',
+    'img/imagenestabla1/Si.jpg',
+    'img/imagenestabla1/Sobre-mi.jpg',
+    'img/imagenestabla1/Te-quiero.jpg',
+    'img/tabla1Mesiento/Asustada.png',
+    'img/tabla1Mesiento/Bien.png',
+    'img/tabla1Mesiento/Cansada.png',
+    'img/tabla1Mesiento/Contenta.png',
+    'img/tabla1Mesiento/Enfadada.png',
+    'img/tabla1Mesiento/Enferma.png',
+    'img/tabla1Mesiento/Mal.png',
+    'img/tabla1Mesiento/Triste.png',
+
+    'img/temas-diversos.jpg',
+    'img/Flecha-gris-arriba-inhabilitada.jpg',
+    'img/Flecha-gris-abajo-off.jpg',
+    'img/Flecha-naranja-abajo.jpg',
+    'img/Flecha-naranja-izquierda.jpg',
+
+
+    'audio/tab1principal/Comoe.mp3',
+    'audio/tab1principal/Gracias.mp3',
+    'audio/tab1principal/Hola.mp3',
+    'audio/tab1principal/Adios.mp3',
+    'audio/tab1principal/Mesiento.mp3',
+    'audio/tab1principal/No.mp3',
+    'audio/tab1principal/Si.mp3',
+    'audio/tab1principal/Sobremi.mp3',
+    'audio/tab1principal/Tequiero.mp3',
+    'audio/Tematica.mp3',
+    'audio/tabla1mesiento/Asustado.mp3',
+    'audio/tabla1mesiento/Bien.mp3',
+    'audio/tabla1mesiento/Cansado.mp3',
+    'audio/tabla1mesiento/Contento.mp3',
+    'audio/tabla1mesiento/Enfadado.mp3',
+    'audio/tabla1mesiento/Enfermo.mp3',
+    'audio/tabla1mesiento/Mal.mp3',
+    'audio/tabla1mesiento/Triste.mp3',
+    'js/app.js',
+    'js/sw-utils.js'
+];
+
+const APP_SHELL_INMUTABLE = [
+   
+    'js/libs/jquery.js'
+];
+
+
+
+self.addEventListener('install', e => {
+
+
+    const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL ));
+
+    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL_INMUTABLE ));
+
+
+
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
+
+});
+
+
+self.addEventListener('activate', e => {
+
+    const respuesta = caches.keys().then( keys => {
+
+        keys.forEach( key => {
+
+            if (  key !== STATIC_CACHE && key.includes('static') ) {
+                return caches.delete(key);
+            }
+
+            if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+
+        });
+
+    });
+
+    e.waitUntil( respuesta );
+
+});
+
+
+
+
+self.addEventListener( 'fetch', e => {
+
+
+    const respuesta = caches.match( e.request ).then( res => {
+
+        if ( res ) {
+            return res;
+        } else {
+
+            return fetch( e.request ).then( newRes => {
+
+                return actualizaCacheDinamico( DYNAMIC_CACHE, e.request, newRes );
+
+            });
+
+        }
+
+    });
+
+
+
+    e.respondWith( respuesta );
+
+});
+
+
